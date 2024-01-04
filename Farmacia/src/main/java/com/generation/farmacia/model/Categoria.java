73b836387050c895c4bf78400f7d65c691f9f7ab
isabella <cisabella626@gmail.com>
2024-01-04 13:18:36 -0300

    Criando CRUD de categora

---
+++ Farmacia/src/main/java/com/generation/farmacia/model/Categoria.java
@@ -0,0 +1,47 @@
+package com.generation.farmacia.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+
+		
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		private Long id;
+		
+		@NotBlank(message = "Escolha o tipo da categoria a qual deseja pesquisar")
+		@Size(min = 5, max = 50, message = "O tipo de categoria ao qual voce dejesa entrar necessita de no minimo 5 e no maximo 50 caracteres")
+		@Column(length = 100)
+		private String tipo;
+
+		
+		
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public String getTipo() {
+			return tipo;
+		}
+
+		public void setTipo(String tipo) {
+			this.tipo = tipo;
+		}
+		
+		
+		
+}
+
